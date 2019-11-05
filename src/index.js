a756fe34c52085f5ee949df5555927258ec9363b
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-11-05 10:24:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const dogTable = document.getElementById("table-body")
+    const dogForm = document.getElementById("dog-form")
 
+    let allDogs = async () => {
+        let response = await fetch ('http://localhost:3000/dogs')
+        let dogs = await response.json()
+        dogs.forEach(dog => {
+            console.log(dog)
+            let dogRow = document.createElement("tr")
+
+            let nameCell = document.createElement("td")
+            nameCell.innerText = dog.name
+            dogRow.appendChild(nameCell)
+
+            let breedCell = document.createElement("td")
+            breedCell.innerText = dog.breed
+            dogRow.appendChild(breedCell)
+
+            let sexCell = document.createElement("td")
+            sexCell.innerText = dog.sex
+            dogRow.appendChild(sexCell)
+
+            let editCell = document.createElement("td")
+            let editButton = document.createElement("button")
+            editButton.innerText = "Edit Dog"
+            editButton.addEventListener("click", () => {
+                dogForm.name.value = dog.name
+                dogForm.breed.value = dog.breed
+                dogForm.sex.value = dog.sex
+
+                dogForm.addEventListener("submit", async (event) => {
+                    event.preventDefault()
+                    let editedDog = await editDog(dog)
+                    nameCell.innerText = editedDog.name
+                    breedCell.innerText = editedDog.breed
+                    sexCell.innerText = editedDog.breed
+                })
+            })
+            editCell.appendChild(editButton)
+            dogRow.appendChild(editCell)
+
+
+            dogTable.appendChild(dogRow)
+        })
+
+        let editDog = async (dog) => {
+            let id = dog.id
+            let formData = {
+                name: event.target.name.value,
+                breed: event.target.breed.value,
+                sex: event.target.sex.value
+            }
+
+            let configObj = {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(formData)
+            }
+
+            let response = await fetch ("http://localhost:3000/dogs/" + id, configObj)
+            let editedDog = await response.json()
+            return editedDog
+        }
+        
+
+    }
+    allDogs()
 })
